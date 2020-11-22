a8d1db4daefaec1a400e45678d0e84e8d3455e76
Rui Fernandes <ruipedro16@protonmail.com>
2020-11-22 01:01:00 +0000

    Guião 6 - Exercício 4

---
+++ Guiao06/src/Exercicio4.java
@@ -0,0 +1,89 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.locks.ReentrantLock;
+
+class Register { // Estado partilhado
+    private ReentrantLock lock = new ReentrantLock();
+    private int n;
+    private int sum = 0;
+
+    public void add(int value) {
+        lock.lock();
+        try {
+            sum += value;
+            n++;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public double avg() {
+        lock.lock();
+        try {
+            if (n < 1) return 0;
+            return (double) sum / n;
+        } finally {
+            lock.unlock();
+        }
+    }
+}
+
+class ServerWorker implements Runnable {
+    private Register register;
+    private Socket socket;
+
+    public ServerWorker(Socket socket, Register register) {
+        this.socket = socket;
+        this.register = register;
+    }
+
+    @Override
+    public void run() {
+        try {
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter out = new PrintWriter(socket.getOutputStream());
+
+            int sum = 0;
+            String line;
+            while ((line = in.readLine()) != null) {
+                try {
+                    int value = Integer.parseInt(line);
+                    sum += value;
+                    register.add(value);
+                } catch (NumberFormatException e) {
+                    // Ignore invalid integers
+                }
+
+                out.println(sum);
+                out.flush();
+            }
+
+            out.println(register.avg());
+            out.flush();
+
+            socket.shutdownOutput();
+            socket.shutdownInput();
+            socket.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+public class Exercicio4 { // Server
+    public static void main(String[] args) throws IOException {
+        ServerSocket ss = new ServerSocket(12345);
+        Register register = new Register();
+
+        while (true) {
+            Socket socket = ss.accept();
+            Thread worker = new Thread(new ServerWorker(socket, register));
+            worker.start();
+        }
+    }
+}
